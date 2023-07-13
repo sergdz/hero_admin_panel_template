69452fb190fe43bb13bf59b214d77f489d7995f7
serdgdz <sergeydzubenko1@gmail.com>
2023-07-13 13:47:30 +0300

    reduxRTK Query

---
+++ src/context/index.js
@@ -0,0 +1,8 @@
+/* import { createContext, useContext } from "react";
+import * as action from '../actions/index';
+
+export const dataContext = createContext(
+    action
+)
+
+export const context = useContext(dataContext) */
